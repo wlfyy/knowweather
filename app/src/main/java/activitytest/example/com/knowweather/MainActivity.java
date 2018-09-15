1be575aaf56cc566cbfa889741d31919175c6af5
wlfyy <1264481056@qq.com>
2018-09-15 21:56:30 +0800

    First commit.

---
+++ app/src/main/java/activitytest/example/com/knowweather/MainActivity.java
@@ -0,0 +1,14 @@
+package activitytest.example.com.knowweather;
+//https://github.com/wlfyy/knowweather.git
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+I:\Users\Administrator.USER-20180718WZ\AndroidStudioProjects\KnowWeather
